8ef6fb2c03ae0304a6666325cca9cac3baa87ffd
jefuentesnava <jesus.e.fuentesnava@gmail.com>
2022-09-06 13:23:33 -0700

    Implemented singleRound logic comparison

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+let playerSelection = prompt("Enter: Rock, Paper, or Scissors. ");
+playerSelection = playerSelection.toLowerCase();
+
 function getComputerChoice(){
-    const choices = ['Rock','Paper','Scissors']
+    const choices = ['rock','paper','scissors']
     let choice = choices[Math.floor(Math.random() * 3)];
-    console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
+function singleRound(playerSelection, getComputerChoice){
+    let result = '';
+    console.log('Player Choice: ' + playerSelection);
+    console.log('Computer Choice: ' + getComputerChoice);
+    if(playerSelection === getComputerChoice){
+        return result = 'No Winner. It\'s a tie';
+    }
+    else if(playerSelection === 'rock'){
+        if(getComputerChoice === 'paper'){
+            return result = 'Computer Wins';
+        }
+        else{
+            return result = 'Player Wins';
+        }
+    }
+    else if(playerSelection === 'paper'){
+        if(getComputerChoice === 'rock'){
+            return result = 'Player Wins';
+        }
+        else{
+            return result = 'Computer Wins';
+        }
+    }
+    else if(playerSelection === 'scissors'){
+        if(getComputerChoice === 'rock'){
+            return result = 'Computer Wins';
+        }
+        else{
+            return result = 'Player Wins';
+        }
+    }
+}
+
+
+console.log(singleRound(playerSelection, getComputerChoice()));
+
+
+
+// getComputerChoice();
 
 /* 
     Game Pseudocode
